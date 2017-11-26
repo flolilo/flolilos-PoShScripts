25eea6511757cef688ef067f9fc8ac16aa56cc4f
flolilo <florian.dolzer@outlook.at>
2017-11-26 14:34:14 +0100

    renamed variables

--- pictures_cleanup.ps1
+++ pictures_cleanup.ps1
@@ -16,7 +16,7 @@
         on the WWW). However, some parts are copies or modifications of very genuine code - see
         the "CREDIT:"-tags to find them.
 
-        .PARAMETER paraInput
+        .PARAMETER $script:InputPath
             Path to check
         .PARAMETER catalog_folder
             Folder that should be excluded
@@ -31,17 +31,18 @@
             None.
 
         .EXAMPLE
-            .\pictures_cleanup.ps1 -paraInput "D:\My pictures" -catalog_Folder "dontdeleteme" -extensions @("*.ext1","*.ext2") -debug 1
+            .\pictures_cleanup.ps1 -$script:InputPath "D:\My pictures" -catalog_Folder "dontdeleteme" -extensions @("*.ext1","*.ext2") -debug 1
 #>
 param(
-    [string]$paraInput="D:\Eigene_Bilder\_CANON",
-    [string]$catalog_folder="_Picture_Catalogs",
-    [array]$extensions=(
+    [string]$InputPath = "D:\Eigene_Bilder\_CANON",
+    [string]$catalog_folder = "_Picture_Catalogs",
+    [array]$extensions = @(
         "*.arw",
         "*.avi",
+        "*.bmp",
         "*.cr2",
         "*.dng",
-        "*.ext",
+        "*.eip",
         "*.hdr",
         "*.j2k",
         "*.jp2",
@@ -64,10 +65,19 @@ param(
     [int]$debug = 1
 )
 
-$confirm = $(if($debug -eq 1){1}else{0})
+[int]$confirm = $(if($debug -eq 1){1}else{0})
 
-# Get all error-outputs in English:
+# DEFINITION: Get all error-outputs in English:
 [Threading.Thread]::CurrentThread.CurrentUICulture = 'en-US'
+# DEFINITION: Hopefully avoiding errors by wrong encoding now:
+$OutputEncoding = New-Object -TypeName System.Text.UTF8Encoding
+
+
+# ==================================================================================================
+# ==============================================================================
+#    Defining generic functions:
+# ==============================================================================
+# ==================================================================================================
 
 # DEFINITION: Making Write-Host much, much faster:
 Function Write-ColorOut(){
@@ -77,26 +87,29 @@ Function Write-ColorOut(){
         .DESCRIPTION
             Using the [Console]-commands to make everything faster.
         .NOTES
-            Date: 2017-10-03
+            Date: 2017-10-30
         
         .PARAMETER Object
-            String to write out
+            String to write out. Mandatory, but will take every non-parametised value.
         .PARAMETER ForegroundColor
             Color of characters. If not specified, uses color that was set before calling. Valid: White (PS-Default), Red, Yellow, Cyan, Green, Gray, Magenta, Blue, Black, DarkRed, DarkYellow, DarkCyan, DarkGreen, DarkGray, DarkMagenta, DarkBlue
         .PARAMETER BackgroundColor
             Color of background. If not specified, uses color that was set before calling. Valid: DarkMagenta (PS-Default), White, Red, Yellow, Cyan, Green, Gray, Magenta, Blue, Black, DarkRed, DarkYellow, DarkCyan, DarkGreen, DarkGray, DarkBlue
         .PARAMETER NoNewLine
             When enabled, no line-break will be created.
+        .PARAMETER Indentation
+            Will move the cursor n blocks to the right, creating a possibility to indent the output without using "    " or "`t".
+
+        .EXAMPLE
+            Just use it like Write-Host.
     #>
     param(
         [Parameter(Mandatory=$true)]
         [string]$Object,
 
-        [Parameter(Mandatory=$false)]
         [ValidateSet("DarkBlue","DarkGreen","DarkCyan","DarkRed","Blue","Green","Cyan","Red","Magenta","Yellow","Black","DarkGray","Gray","DarkYellow","White","DarkMagenta")]
         [string]$ForegroundColor,
 
-        [Parameter(Mandatory=$false)]
         [ValidateSet("DarkBlue","DarkGreen","DarkCyan","DarkRed","Blue","Green","Cyan","Red","Magenta","Yellow","Black","DarkGray","Gray","DarkYellow","White","DarkMagenta")]
         [string]$BackgroundColor,
 
@@ -108,7 +121,7 @@ Function Write-ColorOut(){
 
     if($ForegroundColor.Length -ge 3){
         $old_fg_color = [Console]::ForegroundColor
-        [Console]::ForegroundColor = $ForeGroundColor
+        [Console]::ForegroundColor = $ForegroundColor
     }
     if($BackgroundColor.Length -ge 3){
         $old_bg_color = [Console]::BackgroundColor
@@ -133,140 +146,190 @@ Function Write-ColorOut(){
 }
 
 # DEFINITION: For the auditory experience:
-Function Start-Sound($success){
+Function Start-Sound(){
     <#
         .SYNOPSIS
             Gives auditive feedback for fails and successes
-        
         .DESCRIPTION
             Uses SoundPlayer and Windows's own WAVs to play sounds.
-
         .NOTES
-            Date: 2018-08-22
+            Date: 2018-10-25
 
-        .PARAMETER success
-            If 1 it plays Windows's "tada"-sound, if 0 it plays Windows's "chimes"-sound.
+        .PARAMETER Success
+            1 plays Windows's "tada"-sound, 0 plays Windows's "chimes"-sound.
         
         .EXAMPLE
-            For success: Start-Sound(1)
+            For success: Start-Sound 1
+        .EXAMPLE
+            For fail: Start-Sound 0
     #>
-    $sound = New-Object System.Media.SoundPlayer -ErrorAction SilentlyContinue
-    if($success -eq 1){
-        $sound.SoundLocation = "C:\Windows\Media\tada.wav"
-    }else{
-        $sound.SoundLocation = "C:\Windows\Media\chimes.wav"
+    param(
+        [Parameter(Mandatory=$true)]
+        [int]$Success
+    )
+    try{
+        $sound = New-Object System.Media.SoundPlayer -ErrorAction stop
+        if($Success -eq 1){
+            $sound.SoundLocation = "C:\Windows\Media\tada.wav"
+        }else{
+            $sound.SoundLocation = "C:\Windows\Media\chimes.wav"
+        }
+        $sound.Play()
+    }catch{
+        Write-Output "`a"
     }
-    $sound.Play()
 }
 
-if((Test-Path -LiteralPath $paraInput -PathType Container) -eq $false){
-    Write-ColorOut "Folder non-existent!" -ForegroundColor Red
-    Start-Sleep -Seconds 2
-    Exit
-}
+
+# ==================================================================================================
+# ==============================================================================
+#    Defining specific functions:
+# ==============================================================================
+# ==================================================================================================
 
 [int]$errorcounter = 0
-[int]$changecounter = 100
-$sw_A = [diagnostics.stopwatch]::StartNew()
 
-while($changecounter -ne 0){
-    $changecounter = 0
-    [array]$folder = @()
-    [int]$counter_A = 1
-    $folder = @(Get-ChildItem -LiteralPath $paraInput -Directory | Where-Object {$_.BaseName -notmatch $catalog_folder} | ForEach-Object {
-        if($sw_A.Elapsed.TotalMilliseconds -ge 500 -or $counter_A -eq 1){
-            Write-Progress -Id 1 -Activity "Getting folders..." -Status "# $counter_A - $($_.BaseName)" -PercentComplete -1
+Function Start-Searching(){
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$InputPath,
+        [Parameter(Mandatory=$true)]
+        [string]$catalog_folder,
+        [Parameter(Mandatory=$true)]
+        [array]$extensions
+    )
+
+    $sw_A = [diagnostics.stopwatch]::StartNew()
+    [int]$sw_counter = 0
+    $folder = @(Get-ChildItem -LiteralPath $InputPath -Directory -Recurse | Where-Object {$_.FullName -notmatch $catalog_folder} | ForEach-Object {
+        if($sw_A.Elapsed.TotalMilliseconds -ge 500 -or $sw_counter -eq 0){
+            Write-Progress -Activity "Getting folders..." -Status "# $sw_counter - $($_.FullName)" -PercentComplete -1
             $sw_A.Reset()
             $sw_A.Start()
         }
-        $counter_A++
-        [int]$counter_B = 0
+        $sw_counter++
         [PSCustomObject]@{
             FullName = $_.FullName
             BaseName = $_.BaseName
-            FileCount = @(Get-ChildItem -LiteralPath $_.FullName -File -Include $extensions).count
-
-            [int]$counter_B = 0
-            SubFolder = @(Get-ChildItem -LiteralPath $_.FullName -Directory -Recurse | ForEach-Object {
-                if($sw_A.Elapsed.TotalMilliseconds -ge 500 -or $counter_A -eq 1){
-                    Write-Progress -Id 2 -Activity "    Getting subfolders..." -Status "    # $counter_B - $($_.BaseName)" -PercentComplete -1
-                    $sw_A.Reset()
-                    $sw_A.Start()
-                }
-                $counter_B++
-                [PSCustomObject]@{
-                    FullName = $_.FullName
-                    BaseName = $_.BaseName
-                    FileCount = @(Get-ChildItem -LiteralPath $_.FullName -File -Include $extensions -Recurse).count
-                }
-            })
+            FileCount = 0
+            # SubfolderCount = (Get-ChildItem -Path $_.FullName -Directory).count
         }
     })
-    Write-Progress -Id 1 -Activity "Getting folders..." -Status "Done" -Completed
-    Write-Progress -Id 2 -Activity "    Getting subfolders..." -Status "    Done" -Completed
+    Write-Progress -Activity "Getting folders..." -Status "Done" -Completed
 
-    $folder = $folder | Sort-Object -Property FullName,SubFolder.FullName
-    $folder | Out-Null
-
-    Write-ColorOut "Folder structure:" -ForegroundColor Cyan
+    [int]$sw_counter = 0
     for($i=0; $i -lt $folder.Length; $i++){
-        Write-ColorOut "$($folder[$i].BaseName) - $($folder[$i].filecount)"
-        for($j=0; $j -lt $folder[$i].subfolder.Length; $j++){
-            Write-ColorOut "|---- $($folder[$i].subfolder[$j].BaseName) - $($folder[$i].subfolder[$j].filecount)" -ForegroundColor DarkGray
+        if($sw_A.Elapsed.TotalMilliseconds -ge 500 -or $sw_counter -eq 0){
+            Write-Progress -Activity "Getting files..." -Status "# $sw_counter - $($folder[$i].FullName)" -PercentComplete ($i * 100 / $folder.Length)
+            $sw_A.Reset()
+            $sw_A.Start()
+        }
+        $sw_counter++
+        foreach($j in $extensions){
+            [int]$folder[$i].FileCount += (Get-ChildItem -LiteralPath $folder[$i].FullName -Filter $j -Recurse).Count
         }
     }
-    Start-Sleep -Seconds 5
+    Write-Progress -Activity "Getting files..." -Status "Done" -Completed
+
+
+    $folder = $folder | Sort-Object -Property FullName -Descending
+    $folder | Out-Null
+
+    return $folder
+}
+
+Function Start-Cleaning(){
+    param(
+        [Parameter(Mandatory=$true)]
+        [array]$ToClean,
+        [Parameter(Mandatory=$true)]
+        [string]$InputPath
+    )
 
     Write-ColorOut "Now looking for folders to delete..." -ForegroundColor Cyan
-    for($i=0; $i -lt $folder.Length; $i++){
-        if($folder[$i].subfolder.Length -ne 0){
-            for($j=0; $j -lt $folder[$i].subfolder.Length; $j++){
-                if($folder[$i].subfolder[$j].filecount -eq 0){
-                    Write-ColorOut "$($folder[$i].subfolder[$j].FullName.Replace($paraInput,"."))`t`thas $($folder[$i].subfolder[$j].filecount) files." -ForegroundColor DarkGray
-                    if($confirm -eq 1){
-                        Write-ColorOut (Get-ChildItem -LiteralPath $folder[$i].subfolder[$j].FullName -File -Recurse | Select-Object -ExpandProperty Name)
-                    }
-                    if($confirm -eq 0 -or ($confirm -eq 1 -and (Read-Host "Is that okay? 1 for yes, 0 for no") -eq 1)){
-                        try{
-                            Remove-Item -LiteralPath $folder[$i].subfolder[$j].FullName -Recurse
-                        }catch{
-                            Write-ColorOut "Removing failed!" -ForegroundColor Magenta
-                            $errorcounter++
-                        }
-                        $changecounter++
-                    }
-                }else{
-                    Write-ColorOut "$($folder[$i].subfolder[$j].FullName.Replace($paraInput,"."))`t`thas $($folder[$i].subfolder[$j].filecount) files." -ForegroundColor DarkGreen
+    for($i=0; $i -lt $ToClean.Length; $i++){
+        if($ToClean[$i].filecount -eq 0){
+            Write-ColorOut "$($ToClean[$i].FullName.Replace($InputPath,"."))`t`thas $($ToClean[$i].filecount) files." -ForegroundColor DarkGray
+            if($confirm -eq 1){
+                $inter = Get-ChildItem -LiteralPath $ToClean[$i].FullName -File -Recurse | Sort-Object -Property Extension,BaseName
+                $inter | Format-Table -AutoSize -Property BaseName,Extension,Length
+            }
+            if($confirm -eq 0 -or ($confirm -eq 1 -and (Read-Host "Is that okay? 1 for yes, 0 for no") -eq 1)){
+                try{
+                    Remove-Item -LiteralPath $ToClean[$i].FullName -Recurse -Verbose
+                }catch{
+                    Write-ColorOut "Removing failed!" -ForegroundColor Magenta
+                    $script:errorcounter++
                 }
             }
         }else{
-            if($folder[$i].filecount -eq 0){
-                Write-ColorOut "$($folder[$i].FullName.Replace($paraInput,"."))`t`thas $($folder[$i].subfolder[$j].filecount) files." -ForegroundColor DarkGray
-                if($confirm -eq 1){
-                    Write-ColorOut (Get-ChildItem -LiteralPath $folder[$i].FullName -File -Recurse | Select-Object -ExpandProperty Name)
-                }
-                if($confirm -eq 0 -or ($confirm -eq 1 -and (Read-Host "Is that okay? 1 for yes, 0 for no") -eq 1)){
-                    try{
-                        Remove-Item -LiteralPath $folder[$i].FullName -Recurse
-                    }catch{
-                        Write-ColorOut "Removing failed!" -ForegroundColor Magenta
-                        $errorcounter++
-                    }
-                    $changecounter++
-                }
-            }else{
-                Write-ColorOut "$($folder[$i].FullName.Replace($paraInput,"."))`t`thas $($folder[$i].subfolder[$j].filecount) files." -ForegroundColor DarkGreen
-            }
+            Write-ColorOut "$($folder[$i].FullName.Replace($InputPath,"."))`t`thas $($ToClean[$i].filecount) files." -ForegroundColor DarkGreen
         }
     }
 }
 
-if($errorcounter -eq 0){
-    Write-ColorOut "Done without errors!" -ForegroundColor Green
-    Start-Sound(1)
-}else{
-    Write-ColorOut "Done, though $errorcounter error(s) were encountered." -ForegroundColor Magenta
-    Start-Sound(0)
+Function Start-DocSearch(){
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$InputPath,
+        [Parameter(Mandatory=$true)]
+        [array]$extensions
+    )
+
+    $sw_A = [diagnostics.stopwatch]::StartNew()
+
+    [int]$sw_counter = 0
+    [array]$files = Get-ChildItem -Path $ToClean[$i].FullName -Filter *.xmp -Recurse | ForEach-Object -Process {
+        if($sw_A.Elapsed.TotalMilliseconds -ge 500 -or $sw_counter -eq 0){
+            Write-Progress -Activity "Getting folders..." -Status "# $i - $($ToClean[$i].FullName)" -PercentComplete -1
+            $sw_A.Reset()
+            $sw_A.Start()
+        }
+        $sw_counter++
+
+        [PSCustomObject]@{
+            FullName = $_.FullName
+            BaseName = $_.BaseName
+        }
+    } -End {Write-Progress -Activity "Checking XMPs..." -Status "Done" -Completed}
+
+    for($i=0; $i -lt $files.Length; $i++){
+        if($sw_A.Elapsed.TotalMilliseconds -ge 500 -or $i -eq 0){
+            Write-Progress -Activity "Checking XMPs..." -Status "# $i - $($files[$i].FullName)" -PercentComplete -1
+            $sw_A.Reset()
+            $sw_A.Start()
+        }
+        [int]$counter = 0
+        for($j=0; $j -lt $extensions.Length; $j++){
+            [int]$counter += (Get-ChildItem -Path $ToClean[$i].FullName -Filter $extensions[$j]).count
+        }
+        if($counter -lt 1){
+            Write-ColorOut "Only XMP!" -ForegroundColor Red
+        }else{
+            Write-ColorOut "XMP with file." -ForegroundColor Green
+        }
+    }
+    Write-Progress -Activity "Checking XMPs..." -Status "Done" -Completed
 }
 
-Pause
+Function Start-Everything(){
+    [array]$folder = Start-Searching -InputPath $script:InputPath -catalog_folder $script:catalog_folder -extensions $script:extensions
+    Start-Cleaning -ToClean $folder -InputPath $script:InputPath
+    Start-DocSearch -InputPath $script:InputPath -extensions $script:extensions
+    if($script:errorcounter -le 0){
+        Write-ColorOut "Done without errors!" -ForegroundColor Green
+        Start-Sound -Success 1
+    }else{
+        Write-ColorOut "Done, though $($script:errorcounter) error(s) were encountered." -ForegroundColor Magenta
+        Start-Sound -Success 0
+    }
+}
+
+if((Test-Path -LiteralPath $InputPath -PathType Container) -eq $false){
+    Write-ColorOut "Folder non-existent!" -ForegroundColor Red
+    Start-Sleep -Seconds 5
+    Exit
+}else{
+    Start-Everything
+    Pause
+}
