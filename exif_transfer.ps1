36de2cadac40566ba73843d468f0489d22f8f30c
flolilo <florian.dolzer@outlook.at>
2018-02-17 02:17:27 +0100

    cleaned up functions, batch processing, single file mode.

--- exif_transfer.ps1
+++ exif_transfer.ps1
@@ -6,37 +6,41 @@
     .DESCRIPTION
         Uses exiftool by Phil Harvey (https://sno.phy.queensu.ca/~phil/exiftool/)
     .NOTES
-        Version:        1.0
+        Version:        2.0
         Author:         flolilo
-        Creation Date:  2018-01-27
+        Creation Date:  2018-02-17
 
     .INPUTS
-        exiftool.exe
-        (optional) exif_tool_vars.json, formatted in UTF8 for copyright-values (if not provided via parameters).
+        files.
     .OUTPUTS
-        none.
+        the same files.
 
     .PARAMETER InputPath
         Path where images should be searched and edited (default: current path of console).
+    .PARAMETER Filter
+        Filter for files. E.g. "*IMG_*" for all files with IMG_ in their name.
     .PARAMETER ShowValues
         Show copyright-values before adding them.
+    .PARAMETER EXIFtool
+        Path to Exiftool.exe.
     .PARAMETER Debug
         Add a bit of verbose information about variables.
 
     .EXAMPLE
-        exif_tool -AddCopyright 1 -ArtistName "John Doe" -CopyrightText "2017, by John Doe." -Encoder "C:\exiftool.exe"
+        exif_transfer -Filter "*image_525*" -EXIFtool "C:\exiftool.exe"
 #>
 param(
-    [string]$InputPath =    "$((Get-Location).Path)",
+    [array]$InputPath =     @("$((Get-Location).Path)"),
+    [string]$Filter =       "*",
     [int]$ShowValues =      0,
-    [string]$Encoder =      "$($PSScriptRoot)\exiftool.exe",
+    [string]$EXIFtool =     "$($PSScriptRoot)\exiftool.exe",
     [int]$Debug =           0
 )
 
 # DEFINITION: Get all error-outputs in English:
-    [Threading.Thread]::CurrentThread.CurrentUICulture = 'en-US'
+[Threading.Thread]::CurrentThread.CurrentUICulture = 'en-US'
 # DEFINITION: Hopefully avoiding errors by wrong encoding now:
-    $OutputEncoding = New-Object -TypeName System.Text.UTF8Encoding
+$OutputEncoding = New-Object -TypeName System.Text.UTF8Encoding
 
 
 # ==================================================================================================
@@ -146,6 +150,18 @@ Function Start-Sound(){
     }
 }
 
+# DEFINITION: Pause in Debug:
+Function Invoke-Pause(){
+    if($script:Debug -ne 0){
+        Pause
+    }
+}
+
+# DEFINITION: Getting date and time in pre-formatted string:
+Function Get-CurrentDate(){
+    return $(Get-Date -Format "yy-MM-dd HH:mm:ss")
+}
+
 
 # ==================================================================================================
 # ==============================================================================
@@ -154,55 +170,181 @@ Function Start-Sound(){
 # ==================================================================================================
 
 # DEFINITION: Get user-values:
-Function Start-Transfer(){
-    Write-ColorOut "$(Get-Date -Format "yy.MM.dd - HH:mm")  -" -NoNewLine -ForegroundColor Gray
-    Write-ColorOut "-  Transfering metadata..." -ForegroundColor Cyan
+Function Test-UserValues(){
+    Write-ColorOut "$(Get-CurrentDate)  --  Testing exiftool..." -ForegroundColor Cyan
+
+    # DEFINITION: Search for exiftool:
+    if((Test-Path -LiteralPath $script:EXIFtool -PathType Leaf) -eq $false){
+        if((Test-Path -LiteralPath "$($PSScriptRoot)\exiftool.exe" -PathType Leaf) -eq $true){
+            [string]$script:EXIFtool = "$($PSScriptRoot)\exiftool.exe"
+        }else{
+            Write-ColorOut "Exiftool not found - aborting!" -ForegroundColor Red -Indentation 2
+            Start-Sound -Success 0
+            Start-Sleep -Seconds 5
+            return $false
+        }
+    }
+    if((Test-Path -LiteralPath $script:InputPath -PathType Container) -eq $false){
+        Write-ColorOut "$script:InputPath not found - aborting!" -ForegroundColor Red -Indentation 2
+        Start-Sound -Success 0
+        Start-Sleep -Seconds 5
+        return $false
+    }
+
+    return $true
+}
+
+# DEFINITION: Get user-values:
+Function Get-InputFiles(){
+    param(
+        [Parameter(Mandatory=$true)]
+        [array]$InputPath,
+        [Parameter(Mandatory=$true)]
+        [string]$Filter
+    )
+    Write-ColorOut "$(Get-CurrentDate)  --  Getting files..." -ForegroundColor Cyan
 
     [array]$original = @()
     [array]$jpg = @()
-    [array]$files = @(Get-ChildItem -Path $script:InputPath -File | ForEach-Object {
+    [array]$WorkingFiles = @(Get-ChildItem -Path $script:InputPath -Filter $Filter -File | ForEach-Object {
         [PSCustomObject]@{
             BaseName = $_.BaseName
             FullName = $_.FullName
         }
     })
 
-    [array]$files = @($files | Group-Object -Property BaseName | Where-Object {$_.Count -eq 2})
+    [array]$WorkingFiles = @($WorkingFiles | Group-Object -Property BaseName | Where-Object {$_.Count -gt 1})
 
-    for($i=0; $i -lt $files.Length; $i++){
-            $original += @($files[$i].Group.FullName | Where-Object {$_ -notmatch ".jpg" -and $_ -notmatch ".jpeg"})
-            $jpg += @($files[$i].Group.FullName | Where-Object {$_ -match ".jpg" -or $_ -match ".jpeg"})
+    for($i=0; $i -lt $WorkingFiles.Length; $i++){
+        if($WorkingFiles[$i].Group.FullName.Length -gt 2){
+            [array]$inter = @($WorkingFiles[$i].Group.FullName | Where-Object {$_ -notmatch ".jpg" -and $_ -notmatch ".jpeg"})
+
+            Write-ColorOut "More than one source-file found. Please choose between:" -ForegroundColor Yellow -Indentation 2
+            for($k=0; $k -lt $inter.Length; $k++){
+                Write-ColorOut "$k - $($inter[$k])" -ForegroundColor Gray -Indentation 4
+            }
+            [int]$choice = 999
+            while($choice -notin (0..$inter.Length)){
+                try{
+                    Write-ColorOut "Which one do you want?`t" -ForegroundColor Yellow -NoNewLine -Indentation 2
+                    [int]$choice = Read-Host
+                }catch{
+                    Write-ColorOut "Wrong input!" -ForegroundColor Red -Indentation 4
+                    continue
+                }
+            }
+            $original += @($inter[$choice])
+        }else{
+            $original += @($WorkingFiles[$i].Group.FullName | Where-Object {$_ -notmatch ".jpg" -and $_ -notmatch ".jpeg"})
+        }
+        $jpg += @($WorkingFiles[$i].Group.FullName | Where-Object {$_ -match ".jpg" -or $_ -match ".jpeg"})
     }
 
     for($i=0; $i -lt $original.Length; $i++){
-        Write-ColorOut $original[$i] -ForegroundColor Gray
-        Write-ColorOut $jpg[$i]
+        Write-ColorOut "From:`t$($original[$i].Replace("$script:InputPath","."))" -ForegroundColor Gray -Indentation 4
+        Write-ColorOut "To:`t`t  $($jpg[$i].Replace("$script:InputPath","."))" -Indentation 4
     }
-    Write-ColorOut "`r`nContinue?"
-    Pause
 
+    [array]$WorkingFiles = @()
     for($i=0; $i -lt $original.Length; $i++){
-        Write-Progress -Activity "Transfering metadata..." -Status "# $($i+1) / $($original.Length)" -PercentComplete $($($i + 1) * 100 / $($original.Length))
-        Start-Process -FilePath $script:Encoder -ArgumentList " -tagsfromfile `"$($original[$i])`" -All:All -overwrite_original `"$($jpg[$i])`"" -Wait -NoNewWindow
+        $WorkingFiles += [PSCustomObject]@{
+            SourceFullName = $original[$i]
+            JPEGFullName = $jpg[$i]
+        }
     }
+
+    return $WorkingFiles
+}
+
+Function Start-Transfer(){
+    param(
+        [Parameter(Mandatory=$true)]
+        [array]$WorkingFiles,
+        [Parameter(Mandatory=$true)]
+        [string]$EXIFtool
+    )
+    Write-ColorOut "$(Get-CurrentDate)  --  Transfering metadata..." -ForegroundColor Cyan
+    $sw = [diagnostics.stopwatch]::StartNew()
+    if($script:Debug -gt 0){
+        [string]$debuginter = "$((Get-Location).Path)"
+    }
+
+    # DEFINITION: Create Exiftool process:
+    $psi = New-Object System.Diagnostics.ProcessStartInfo
+    $psi.FileName = $script:EXIFtool
+    $psi.Arguments = "-stay_open True -@ -"
+    $psi.UseShellExecute = $false
+    $psi.RedirectStandardInput = $true
+    $psi.RedirectStandardOutput = $true
+    $psi.RedirectStandardError = $true
+    $exiftoolproc = [System.Diagnostics.Process]::Start($psi)
+    Start-Sleep -Seconds 1
+
+    # DEFINITION: Pass arguments to Exiftool:
+    for($i=0; $i -lt $WorkingFiles.length; $i++){
+        if($sw.Elapsed.TotalMilliseconds -ge 750){
+            Write-Progress -Activity "Transfering metadata..." -Status "File # $i - $($WorkingFiles[$i].jpg)" -PercentComplete $($i * 100 / $WorkingFiles.length)
+            $sw.Reset()
+            $sw.Start()
+        }
+
+        [string]$ArgList = "-tagsfromfile`n$($WorkingFiles[$i].SourceFullName)`n-All:All`n-xresolution=300`n-yresolution=300`n-overwrite_original`n-progress`n$($WorkingFiles[$i].JPEGFullName)"
+        if($script:Debug -gt 0){
+            Write-ColorOut $ArgList.Replace("`n"," ").Replace("$debuginter",".") -ForegroundColor DarkGray -Indentation 4
+        }
+        $exiftoolproc.StandardInput.WriteLine("$ArgList`n-execute`n")
+    }
+    $exiftoolproc.StandardInput.WriteLine("-stay_open`nFalse`n")
+
+    [array]$outputerror = @($exiftoolproc.StandardError.ReadToEnd().Split("`r`n",[System.StringSplitOptions]::RemoveEmptyEntries))
+    [string]$outputout = $exiftoolproc.StandardOutput.ReadToEnd()
+    $outputout = $outputout -replace '========\ ','' -replace '\[1/1]','' -replace '\ \r\n\ \ \ \ '," - " -replace '{ready}\r\n',''
+    [array]$outputout = @($outputout.Split("`r`n",[System.StringSplitOptions]::RemoveEmptyEntries))
+
+    $exiftoolproc.WaitForExit()
+    Write-Progress -Activity "Transfering metadata..." -Status "Complete!" -Completed
+
+    for($i=0; $i -lt $WorkingFiles.length; $i++){
+        Write-ColorOut "$($WorkingFiles[$i].JPEGFullName.Replace("$script:InputPath",".")):`t" -ForegroundColor Gray -NoNewLine -Indentation 2
+        if($outputerror[$i].Length -gt 0){
+            Write-ColorOut "$($outputerror[$i])`t" -ForegroundColor Red -NoNewline
+        }
+        Write-ColorOut "$($outputout[$i])" -ForegroundColor Yellow
+    }
+
     Write-Progress -Activity "Transfering metadata..." -Status "Done!" -Completed
 }
 
 # DEFINITION: Start everything:
 Function Start-Everything(){
-    Write-ColorOut "Welcome to flolilo's exif-tool!" -ForegroundColor DarkCyan -BackgroundColor Gray
+    Write-ColorOut "                                              A" -BackgroundColor DarkGray -ForegroundColor DarkGray
+    Write-ColorOut "         flolilo's EXIF transfer tool          " -ForegroundColor DarkCyan -BackgroundColor Gray
+    Write-ColorOut "               v2.0 - 2018-02-16               " -ForegroundColor DarkCyan -BackgroundColor Gray
+    Write-ColorOut "(PID = $("{0:D8}" -f $pid))                               `r`n" -ForegroundColor Gray -BackgroundColor DarkGray
 
-    if((Test-Path -LiteralPath $script:InputPath -PathType Container) -eq $true){
-        Start-Transfer
-        Write-ColorOut "$(Get-Date -Format "yy.MM.dd - HH:mm")  -" -NoNewLine -ForegroundColor Gray
-        Write-ColorOut "-  Done!" -ForegroundColor Green
-        Start-Sound -Success 1
-    }else{
-        Write-ColorOut "Path not found - aborting!" -ForegroundColor Red
-        Start-Sound -Success 0
-        Start-Sleep -Seconds 5
+    if((Test-UserValues) -eq $false){
+        Invoke-Pause
         Exit
     }
+    Invoke-Pause
+
+    [array]$WorkingFiles = @(Get-InputFiles -InputPath $script:InputPath -Filter $script:Filter)
+    if($WorkingFiles.Length -lt 1){
+        Write-ColorOut "No files found!" -ForegroundColor Magenta
+        Start-Sound -Success 1
+        Start-Sleep -Seconds 5
+        Invoke-Pause
+        Exit
+    }
+    Write-ColorOut "Continue?`t" -ForegroundColor Yellow -NoNewLine -Indentation 2
+    Pause
+
+    Start-Transfer -WorkingFiles $WorkingFiles -EXIFtool $script:EXIFtool
+
+    Write-ColorOut "$(Get-CurrentDate)  --  Done!" -ForegroundColor Green
+    Start-Sound -Success 1
+    Start-Sleep -Seconds 5
+    Invoke-Pause
 }
 
 Start-Everything
