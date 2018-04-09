279eaa40b2dff65e1b59586cde0d0d82a772286b
flolilo <florian.dolzer@outlook.at>
2018-04-09 22:04:01 +0200

    1.2 - added backup, added "0pen with"

--- system_tools/remove_registry_entries.ps1
+++ system_tools/remove_registry_entries.ps1
@@ -6,8 +6,8 @@
     .DESCRIPTION
         Please be careful with using this tool!
     .NOTES
-        Version:        1.1
-        Creation Date:  2017-10-09
+        Version:        1.2
+        Creation Date:  2018-04-09
         Legal stuff: This program is free software. It comes without any warranty, to the extent permitted by
         applicable law. Most of the script was written by myself (or heavily modified by me when searching for solutions
         on the WWW). However, some parts are copies or modifications of very genuine code - see
@@ -30,9 +30,15 @@ if((([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::
     [switch]$wantverbose = $(if((Read-Host "Want some verbose? (To see what's happening)`t") -eq 1){$true}else{$false})
     [switch]$wantwhatif = $(if((Read-Host "Activate dry-running? (Only show what would happen, not actually doing anything)`t") -eq 1){$true}else{$false})
     Write-Host "`r`nThis script will change values in your system's Registry, so perhaps it would be a good moment to export (save) your Registry NOW.`r`n" -ForegroundColor Magenta
-    Write-Host "To do this, press <Win>+<R>, type in 'regedit' (w/o quotes) go to 'File'->'Export', choose `"everything`" and export it to e.g. your Desktop."
-    Write-Host "After the following pause, the script will start (however, you will be again asked for each step." -ForegroundColor Magenta
-    Pause
+    if((Read-Host "Backup registry now?") -eq 1){
+        try{
+            [string]$inter = Read-Host "Please specify path (and name) to save .reg-file at:`t"
+            Start-process regedit -ArgumentList "/e $($inter).reg" -Wait -NoNewWindow
+        }catch{
+            Write-Host "Could not backup registry!" -ForegroundColor Magenta
+            Pause
+        }
+    }
 }
 
 # DEFINITION: Create HKCR as drive for PS:
@@ -153,6 +159,43 @@ if((Read-Host "Remove GitKraken from context menu?") -eq 1){
         }
     }else{
         Write-Host "Could not find $regpath" -ForegroundColor Magenta
+    }
+}
+
+# DEFINITION: Add '0pen with' on top of context menu:
+# CREDIT: https://superuser.com/questions/1281615
+[int]$inter = Read-Host "Add/remove '0pen with' on top of context menu? 1 = `"add`", 2 `"remove`", 0 = `"I'm fine, thanks`"."
+if($inter -eq 1){
+    Write-Host "Add '0pen with' on top of context menu..." -ForegroundColor Cyan
+    [string]$regpath = "HKCR:\*\shell\0pen With"
+    if((Test-Path -LiteralPath $regpath) -eq $false){
+        try{
+            New-Item -Path $regpath -Verbose:$wantverbose -WhatIf:$wantwhatif
+            Write-Host "Added key $regpath" -ForegroundColor DarkGreen
+            New-Item -Path "$regpath\command" -Verbose:$wantverbose -WhatIf:$wantwhatif
+            Write-Host "Added key $regpath\command" -ForegroundColor DarkGreen
+        }catch{
+            Write-Host "Could not create key $regpath or $regpath\command" -ForegroundColor Red
+            $myerror=1
+        }
+    }
+    if($myerror -ne 1){
+        try{
+            New-ItemProperty -LiteralPath $regpath -Name "Position" -Value "Top" -PropertyType String -Verbose:$wantverbose -WhatIf:$wantwhatif
+            Write-Host "Added String `"Postition=Top`"" -ForegroundColor DarkGreen
+            New-ItemProperty -LiteralPath "$regpath\command" -Name "(Default)" -Value "{09799AFB-AD67-11d1-ABCD-00C04FC30936}" -PropertyType String -Verbose:$wantverbose -WhatIf:$wantwhatif
+            Write-Host "Added String `"(Default)={09799AFB-AD67-11d1-ABCD-00C04FC30936}`"" -ForegroundColor DarkGreen
+        }catch{
+            Write-Host "Could not create Strings in $regpath" -ForegroundColor Red
+        }
+    }
+}elseif($inter -eq 2){
+    [string]$regpath = "HKCR:\*\shell\0pen With"
+    try{
+        Remove-Item -LiteralPath $regpath -Recurse -Verbose:$wantverbose -WhatIf:$wantwhatif
+        Write-Host "Deleted $regpath" -ForegroundColor DarkGreen
+    }catch{
+        Write-Host "Could not delete $regpath" -ForegroundColor Red
     }
 }
 
