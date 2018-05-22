fe9e8472b72c58ac23a9282918c16b6a3486666a
flolilo <florian.dolzer@outlook.at>
2018-05-22 11:16:41 +0200

    updated start-singlecopy

--- system_tools/profile.ps1
+++ system_tools/profile.ps1
@@ -12,9 +12,9 @@ Function Start-SingleCopy(){
         .DESCRIPTION
             Using Robocopy and Get-FileHash to copy a file.
         .NOTES
-            Version:        0.1.1 (Beta)
+            Version:        0.1.2
             Author:         flolilo
-            Creation Date:  2017-12-02
+            Creation Date:  2018-05-22
             Legal stuff: This program is free software. It comes without any warranty, to the extent permitted by
             applicable law.
 
@@ -29,55 +29,44 @@ Function Start-SingleCopy(){
             Start-SingleCopy "D:\My Files\test.txt" D:\
     #>
     param(
-        [Parameter(Mandatory=$true)]
         [ValidateScript({Test-Path -LiteralPath $_ -PathType Leaf})]
-        [string]$InputFile,
-        [Parameter(Mandatory=$true)]
+        [string]$InputFile = $(throw '-InputFile is needed by Start-SingleCopy to work!'),
         [ValidateScript({Test-Path -LiteralPath $_ -PathType Container})]
-        [string]$OutputPath,
-        <#
-            [Parameter(Mandatory=$false)]
-            [ValidateSet("none","yyyy-MM-dd","yyyy-MM-dd_HH-mm-ss","yyyy-MM-dd","yyyy-MM-dd_HH-mm-ss","yyyy_MM_dd","yyyy_MM_dd-HH_mm_ss","yy_MM_dd","yy_MM_dd-HH_mm_ss")]
-            [string]$NameStyle = "none",
-        #>
-        [Parameter(Mandatory=$false)]
+        [string]$OutputPath = $(throw '-OutputPath is needed by Start-SingleCopy to work!'),
         [ValidateRange(0,1)]
         [int]$Overwrite = 0
     )
+    $InputFile = Resolve-Path -LiteralPath $InputFile
+    $OutputPath = Resolve-Path -LiteralPath $OutputPath
 
     if($Overwrite -eq 0){
-        if((Test-Path -LiteralPath "$OutputPath$(Split-Path -Path $InputFile -Leaf)" -PathType Leaf) -eq $true){
-            $inter = Read-Host "`"$OutputPath$(Split-Path -Path $InputFile -Leaf)`" already present - overwrite?"
-            if($inter -ne 1 -and $inter -ne "y"){
+        if((Test-Path -LiteralPath "$OutputPath\$(Split-Path -Path $InputFile -Leaf)" -PathType Leaf) -eq $true){
+            [int]$inter = 0
+            [int]$inter = Read-Host "`"$OutputPath\$(Split-Path -Path $InputFile -Leaf)`" already present - overwrite?"
+            if($inter -ne 1){
                 [System.Console]::WriteLine("Aborting!")
                 return
             }
         }
     }
 
-    $hashIn = (Get-FileHash -LiteralPath $InputFile -Algorithm SHA1 | Select-Object -ExpandProperty Hash)
+    [string]$hashIn = (Get-FileHash -LiteralPath $InputFile -Algorithm SHA1).Hash # | Select-Object -ExpandProperty Hash)
 
-    [string]$RC_OutputPath = $(
-        if($OutputPath.Substring($OutputPath.Length-1) -eq "\"){
-            "`"$($OutputPath.Substring(0,$OutputPath.Length-1))`""
-        }else{
-            "`"$OutputPath`""
-        }
-    )
-    [string]$RC_InputPath = "`"$(Split-Path -Path $InputFile -Parent)`""
-    [string]$RC_InputName = "`"$(Split-Path -Path $InputFile -Leaf)`""
-    [string]$ArgList = "$RC_InputPath $RC_OutputPath $RC_InputName /R:5 /W:15 /NJH /NJS /NC /J"
+    [string]$RC_OutputPath = $OutputPath
+    [string]$RC_InputPath = $(Split-Path -Path $InputFile -Parent)
+    [string]$RC_InputName = $(Split-Path -Path $InputFile -Leaf)
+    [string]$ArgList = "`"$RC_InputPath`" `"$RC_OutputPath`" `"$RC_InputName`" /R:3 /W:5 /NJH /NJS /NC /J"
 
     while($true){
         Start-Process robocopy -ArgumentList $ArgList -Wait -NoNewWindow
-        Start-Sleep -Seconds 1
-        $hashOut = (Get-FileHash -LiteralPath "$OutputPath$(Split-Path -Path $InputFile -Leaf)" -Algorithm SHA1 | Select-Object -ExpandProperty Hash)
+        Start-Sleep -Milliseconds 250
+        [string]$hashOut = (Get-FileHash -LiteralPath "$OutputPath\$(Split-Path -Path $InputFile -Leaf)" -Algorithm SHA1 | Select-Object -ExpandProperty Hash)
         if(@(Compare-Object -ReferenceObject $hashIn -DifferenceObject $hashOut).Count -gt 0){
-            [System.Console]::WriteLine("Input-Hash $hashIn != Output-Hash $hashOut")
-            Start-Sleep -Seconds 1
+            [System.Console]::WriteLine("Retry:`t$hashIn != $hashOut")
+            Start-Sleep -Milliseconds 750
             continue
         }else{
-            [System.Console]::WriteLine("Done!")
+            [System.Console]::WriteLine("Done!`t$hashIn = $hashOut")
             break
         }
     }
